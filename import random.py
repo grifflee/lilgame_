fb5dc8257b2918050d923cb9d14a374a1633f823
Griffen Lee <1981644078+grifflee@users.noreply.github.com>
2026-01-02 17:16:45 -0600

    update

--- import random.py
+++ import random.py
@@ -1,10 +1,17 @@
 import random
 
+# break = destroy the loop and move to the next block of code (outside of loop)
+# continue = skip the rest of current loop, and go back to the start of loop
+# pass = does nothing but correctly close off the syntax so no error
+
+
+
 class Character:
     def __init__(self, name, hp):
         self.name = name
         self.hp = hp
         self.max_hp = hp #save the originally set HP as the max_hp
+        self.potions = 3 #potions default to 3
 
     def attack(self, other):
         damage = random.randint(5, 10)
@@ -12,16 +19,28 @@ class Character:
         print(f"{self.name} attacked {other.name} for {damage} damage!")
 
     def heal(self):
-        #random healing amount
-        heal_amount = random.randint(5, 10)
-        #add to the current hp
-        self.hp += heal_amount
 
-        if self.hp > self.max_hp:
-            self.hp = self.max_hp
+        if self.potions > 0:
+            #random healing amount
+            heal_amount = random.randint(5, 10)
+            #add to the current hp
+            self.hp += heal_amount
 
-        print(f"{self.name} healed for {heal_amount} HP! (Current HP: {self.hp}/{self.max_hp})")
+            if self.hp > self.max_hp:
+                self.hp = self.max_hp
 
+            #remove one used potion from inventory
+            self.potions -= 1
+
+            
+            print(f"{self.name} used a potion and healed for {heal_amount} HP! (Current HP: {self.hp}/{self.max_hp})")
+            print(f"You used a potion, number of potions remaining: {self.potions}")
+            return True
+        
+        else:
+            print("You reach for a potion but your bag is empty!")
+            return False
+        
 #initalize the two characters
 player = Character ("Hero", 100)
 enemy = Character ("Goblin", 100)
@@ -41,7 +60,11 @@ while player.hp > 0 and enemy.hp > 0:
     if choice == "1":
         player.attack(enemy)
     elif choice == "2":
-        player.heal()
+        #only let enemy attack if the heal was successful 
+        if player.heal():
+            pass #the turn happened 
+        else: 
+            continue
     else: 
         print("You panicked and missed your turn")
 
